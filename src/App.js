7456c1026808e69a03d1d356267ba4d39cad51f0
Diana Aviles <120342520+octomiau@users.noreply.github.com>
2023-10-11 22:57:39 -0500

    comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import TransactionsList from './admin';
+import TransactionsList from './admin';  // Importing the TransactionsList component from the admin file.
 
+// The main App component of the application.
 function App() {
     return (
+        // Root container for the app with a top margin.
         <div className="container mt-4">
+            {/* Displaying a warning heading. */}
             <h1>WARNING!</h1>
+            
+            {/* Rendering the TransactionsList component which shows a list of all transactions. */}
             <TransactionsList />
         </div>
     );
 }
 
-export default App;
+export default App;  // Exporting the App component for use in other parts of the application.
